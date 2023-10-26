c38d588a57a22ca456b3ba6fa4fc7236e9a843c2
Ravali777 <85568373+Ravali777@users.noreply.github.com>
2023-10-26 16:43:40 +0100

    while in zug zuruck

--- MVC/src/mvc/AnwendersystemModel.java
+++ MVC/src/mvc/AnwendersystemModel.java
@@ -2,4 +2,7 @@ package mvc;
 import java.io.*;
 public class AnwendersystemModel {
 	public AnwendersystemModel() {} 
+	public String getUeberschrift() {
+		return "Hello";
+	}
 }
--- MVC/src/mvc/AnwendersystemView.java
+++ MVC/src/mvc/AnwendersystemView.java
@@ -1,15 +1,33 @@
 package mvc;
 
+import javafx.event.*;
+import javafx.geometry.*;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class AnwendersystemView {
 	private AnwendersystemControl anwControl;
 	private AnwendersystemModel anwModel;
-	
-	public AnwendersystemView(AnwendersystemControl anwControl,Stage primaryStage , AnwendersystemModel anwModel) {
+
+	private GridPane grid = new GridPane();
+	private TextField txtfl = new TextField();
+	private Button btnTest = new Button("Test");
+
+	public AnwendersystemView(AnwendersystemControl anwControl, Stage primaryStage, AnwendersystemModel anwModel) {
+		this.anwControl = anwControl;
+		this.anwModel = anwModel;
+		this.grid.setAlignment(Pos.CENTER);
+		this.grid.setHgap(10);
+		this.grid.setVgap(10);
+		this.grid.setPadding(new Insets(25,25,25,25));
+		primaryStage.setTitle(
+				this.anwModel.getUeberschrift());
+		Scene scene =new Scene(grid,350,150);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
-	
-	
-	
+
 }
